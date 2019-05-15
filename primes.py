f38ec33a9d50353e1bdc6a8b703fbae7cc62958f
Christina Ling <clingc7@gmail.com>
2019-05-15 11:49:52 -0700

    Add input validation and adjust formatting

--- primes.py
+++ primes.py
@@ -1,49 +1,43 @@
-
-class MultiplicationTable():
+class MultiplicationTable:
+    """A multiplication table class."""
 
     def __init__(self):
-        """Instantiates an empty multiplication table."""
+        """Instantiate an empty multiplication table."""
+
         self.table = []
 
-    def get_n_input(self):
-        """Gets the specified input of n prime numbers from the user."""
-        n = input("How many prime numbers would you like in the multiplication table? ")
-        try:
-            n = int(n)
-            return n
-        except:
-            print('Sorry, that\'s not a valid integer. Please restart the program.')
+    def create_primes_table(self, n):
+        """Create a 2D array of n primes and their products."""
 
-    def create_primes_table(self, n_primes):
-        """Generates a 2D array of n primes and their products."""
+        # validate n
+        if n == 0:
+            print('You\'ve selected 0 primes. Here is a nonexistent table.')
+        if not isinstance(n, int) or n < 0:
+            print('Sorry, that\'s not a valid number of primes.')
+            print('Please try again with an integer greater than 0.')
+        else:
+            # generate first row of table of n primes
+            n_primes = get_n_primes(n)
+            self.table.append(n_primes)
 
-        # n = self.get_n_input()
-        n_primes = get_n_primes(10)
+            for i in range(1, len(n_primes)):
+                row = []
+                row.append(n_primes[i])
+                for j in range(1, len(n_primes)):
+                    product = int(n_primes[i]) * int(n_primes[j])
+                    row.append(str(product))
+                self.table.append(row)
 
-        table = []
-        table.append(n_primes)
+    def print_primes_table(self, n):
+        """Print the multiplication table to stdout."""
 
-        for i in range(1, len(n_primes)):
-            row = []
-            row.append(n_primes[i])
-            for j in range(1, len(n_primes)):
-                row.append(str(int(n_primes[i]) * int(n_primes[j])))
-            table.append(row)
-
-        return table
-
-    def print_primes_table(self, n_primes):
-        """Prints the mulitplication table of n prime numbers to stdout."""
-
-        primes_table = self.create_primes_table(n_primes)
-
-        for row in primes_table:
+        primes_table = self.create_primes_table(n)
+        for row in self.table:
             print('\t'.join(row))
 
 
-
 def is_prime(num):
-    """Returns a boolean of whether the given number is prime."""
+    """Return a boolean of whether num is prime."""
 
     if num == 2:
         return True
@@ -54,25 +48,24 @@ def is_prime(num):
 
 
 def get_n_primes(n):
-    """Returns a list of the first n prime numbers."""
+    """Return a list of the first n prime numbers."""
 
     primes = [' ']
     num = 2
-
     while len(primes) < n + 1:
         if is_prime(num):
             primes.append(str(num))
         num += 1
-
     return primes
 
 
 if __name__ == '__main__':
-    print("* * * * * * * * * *")
-    print("\nFunding Circle Coding Challenge")
-    print("\nThis program prints out a multiplication table of the first 10 prime numbers. Try it with your own input as well!")
-    print("\n\n* * * * * * * * * *")
+    print('* * * * * * * * * *\n')
+    print('Funding Circle Coding Challenge\n')
+    print('This program prints out a multiplication table of the first 10 prime numbers.')
+    print('Try it with another value of n as well.\n')
+    print('* * * * * * * * * *\n')
+
+    n = 8
     table = MultiplicationTable()
-    table.print_primes_table(10)
-    # custom_table = MultiplicationTable()
-    # custom_table.print_primes_table()
+    table.print_primes_table(n)
